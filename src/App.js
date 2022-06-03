de94ecf1befc29cfade8a0c86e7885f7056ab7ba
Niloy Saha <studnil4u@gmail.com>
2022-06-03 15:11:51 +0530

    initial commit with header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import './assets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <Container fluid>
+        <h1>Hello There</h1>
+      </Container>
+    </Fragment>
   );
 }
 
