5d0c42baea0fd818c6f8c8b74c3b9b24b5d28c9c
Niloy Saha <studnil4u@gmail.com>
2022-06-04 02:15:38 +0530

    image onError handle

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faThumbsUp, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 import { Card } from "react-bootstrap";
+import logo from '../assets/loader.png'
 
 const MovieItem = ({ item }) => {
   const format = (num) => {
@@ -12,11 +13,16 @@ const MovieItem = ({ item }) => {
   };
 
   return (
-    <Card className='bg-dark text-white mb-4'>
+    <Card className='bg-dark text-white mb-4' style={{minHeight: '30.8rem'}}>
       <Card.Img
         src={`${process.env.REACT_APP_TMDB_IMAGE_BASE}w780/${item.poster_path}`}
         alt='Card image'
         style={{ height: "100%" }}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null; // prevents looping
+          currentTarget.src=logo;
+          currentTarget.style = 'height: 100%; padding-bottom: 182px; padding-top: 200px; padding-left: 104px; width: fit-content;'
+        }}
       />
       <Card.ImgOverlay className='mb-4'></Card.ImgOverlay>
       <Card.Footer className='d-flex flex-row justify-content-around'>
