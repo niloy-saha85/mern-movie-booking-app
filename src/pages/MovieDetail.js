aedb7d9a0d8e4c75f92075637b8d17f8509f9915
Niloy Saha <studnil4u@gmail.com>
2022-06-07 21:29:46 +0530

    finishing up the details page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import { Card, Col, Container, Image, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { selectCredits, selectDetails } from "../store/reducers/details";
 import { LOAD_CREDITS, LOAD_DETAILS } from "../store/types";
 // import Image from "react-bootstrap/Image";
@@ -11,6 +11,7 @@ import { format, timeConvert } from "../helpers/common";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { Button } from "react-bootstrap";
 
 const MovieDetail = () => {
   const params = useParams();
@@ -54,7 +55,9 @@ const MovieDetail = () => {
                 <div className='d-flex flex-row justify-content-between'>
                   <div className='d-flex flex-column'>
                     <h1>{details.title}</h1>
-                    <Card.Text className="d-none d-lg-block">{details.overview}</Card.Text>
+                    {/* <Card.Text className='d-none d-lg-block'>
+                      {details.overview}
+                    </Card.Text> */}
                     <div>
                       <FontAwesomeIcon
                         className='align-middle'
@@ -84,6 +87,9 @@ const MovieDetail = () => {
                         </li>
                       </ul>
                     </div>
+                    <div className='mt-2'>
+                      <Button variant="danger" size="lg" as={Link} to={`/movie/${details.id}/book`} >Book Now</Button>
+                    </div>
                   </div>
                   <div>
                     <Image
@@ -104,32 +110,55 @@ const MovieDetail = () => {
             </Card>
           </Col>
         </Row>
+        <Row className="mt-2">
+          <Col>
+            <h3>About the movie</h3>
+            <Row>
+              <Col>
+                <div>
+                  <p className="text-muted">{details.overview}</p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
         <hr />
         <Row>
           <Col>
-            <h3>Cast</h3>
+            <h3>Cast { credits && credits.cast && `(${credits.cast.length})` }</h3>
             <Row className='d-flex flex-row'>
               {credits &&
                 credits.cast &&
                 credits.cast.map((c, i) => {
-                  if (i > 11) return null;
+                  // if (i > 11) return null;
                   return (
-                    <Col
-                      className=''
-                      key={c.id}
-                    >
-                      <Image
-                       className="align-middle"
-                       fluid
-                       roundedCircle
-                        src={`${process.env.REACT_APP_TMDB_IMAGE_BASE}w45/${c.profile_path}`}
-                        onError={({ currentTarget }) => {
-                          currentTarget.onerror = null; // prevents looping
-                          currentTarget.src = logo;
-                          currentTarget.style = "width: 45px; height: 67px; object-fit: contain; ";
+                    <Col md={2} key={c.id} className='mb-4'>
+                      <div
+                        style={{
+                          width: "185px",
+                          height: "278px",
+                          borderRadius: "50%",
+                          objectFit: "contain",
+                          margin: "0 auto",
+                          marginBottom: "6px",
                         }}
-                      />
-                      <div>{c.name}</div>
+                      >
+                        <Image
+                          className='align-middle'
+                          // fluid
+                          roundedCircle
+                          src={`${process.env.REACT_APP_TMDB_IMAGE_BASE}w185/${c.profile_path}`}
+                          onError={({ currentTarget }) => {
+                            currentTarget.onerror = null; // prevents looping
+                            currentTarget.src = logo;
+                            currentTarget.style =
+                              "width: 185px; height: 278px; object-fit: contain; ";
+                          }}
+                        />
+                        <div className='text-truncate text-center text-muted'>
+                          {c.name}
+                        </div>
+                      </div>
                     </Col>
                   );
                 })}
